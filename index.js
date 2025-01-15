30b0a26738c612de6bab5a2d39eefd8350b8560f
jimmy4701 <jeremy.simonklein@gmail.com>
2025-01-15 18:03:25 +0100

    Ajout du refresh

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ let cookies_per_second = 0
 
 const logo_cookie = document.getElementById('logo-cookie')
 const total_cookie_container = document.getElementById('total-cookie')
+const cookies_per_second_container = document.getElementById('cps-container')
+
+const refresh = () => {
+    total_cookie_container.innerText = "Total cookies : " + total_cookie
+    cookies_per_second_container.innerText = "Cookies par seconde : " + cookies_per_second
+}
 
 const addCookie = (nbCookies) => {
     total_cookie += nbCookies
-    total_cookie_container.innerText = "Total cookies : " + total_cookie
+    refresh()
 }
 
 logo_cookie.addEventListener("click", () => addCookie(1))
