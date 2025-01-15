ea8f3c1aeb654b43396dc18a984a6d66aceb0639
jimmy4701 <jeremy.simonklein@gmail.com>
2025-01-15 17:55:32 +0100

    Last hour code

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-let total_cookie = 0 
+let total_cookie = 0
+let cookies_per_second = 0
 
 const logo_cookie = document.getElementById('logo-cookie')
 const total_cookie_container = document.getElementById('total-cookie')
 
-const addCookie = () => {
-    total_cookie++
+const addCookie = (nbCookies) => {
+    total_cookie += nbCookies
     total_cookie_container.innerText = "Total cookies : " + total_cookie
 }
 
-logo_cookie.addEventListener("click", addCookie)
+logo_cookie.addEventListener("click", () => addCookie(1))
+
+setInterval(() => addCookie(cookies_per_second), 1000)
+
 
 
